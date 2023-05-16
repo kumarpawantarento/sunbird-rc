78a0bbc30601b4f7d6b1a7d16c750852fc251258
soham <soham.bakshi@tarento.com>
2023-05-16 14:58:56 +0530

    csv upload

---
+++ java/registry/src/main/java/dev/sunbirdrc/registry/dao/StudentDTO.java
@@ -0,0 +1,20 @@
+package dev.sunbirdrc.registry.dao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDTO {
+    @JsonProperty("rollNo")
+    private String rollNo;
+
+    @JsonProperty("dob")
+    private String dob;
+
+}
