254cb9dc660c80a0dbe2b08eb6e6ce63cf1c3394
Pawan k <128481755+kumarpawantarento@users.noreply.github.com>
2023-07-10 15:05:12 +0530

    mertics change

--- services/metrics/config/config.go
+++ services/metrics/config/config.go
@@ -17,7 +17,7 @@ var Config = struct {
 		Database string `env:"CLICKHOUSE_DATABASE" yaml:"database" default:"default"`
 	}
 	Kafka struct {
-		BootstrapServers    string `env:"KAFKA_BOOTSTRAP_SERVERS" yaml:"bootstrapServers" default:"localhost:9094"`
+		BootstrapServers    string `env:"KAFKA_BOOTSTRAP_SERVERS" yaml:"bootstrapServers" default:"kafka:9094"`
 		KAFKA_METRICS_TOPIC string `env:"KAFKA_METRICS_TOPIC" yaml:"metricsTopic" default:"metrics"`
 	}
 	Database struct {
